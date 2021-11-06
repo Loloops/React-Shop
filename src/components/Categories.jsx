ea21873ef7bd4207e1602be92c0013d596ce277d
V <sss.5151@mail.ru>
2021-11-06 18:06:34 +0400

    add reactRouterDom and filter component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,18 +3,25 @@ import { useState } from 'react';
 function Categories({ items, onClickItem }) {
   const [activeItem, setActiveItem] = useState(null);
 
+  const onSelectitem = (index) => {
+    setActiveItem(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li>Все</li>
-        {items.map((item, index) => (
-          <li
-            className={activeItem === index ? 'active' : ''}
-            onClick={() => setActiveItem(index)}
-            key={`${item}_${index}`}>
-            {item}
-          </li>
-        ))}
+        <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectitem(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((item, index) => (
+            <li
+              className={activeItem === index ? 'active' : ''}
+              onClick={() => onSelectitem(index)}
+              key={`${item}_${index}`}>
+              {item}
+            </li>
+          ))}
       </ul>
     </div>
   );
